b4d212d47e05ff79f038628dbfc41154c5df4f4b
Chani Andrianarinaivo <chaniandrianarinaivo@gmail.com>
2025-05-19 10:18:11 +0100

    test(niveauEtude):test sur le controller de l'entity Niveau etude effectue.

--- src/Entity/NiveauEtude.php
+++ src/Entity/NiveauEtude.php
@@ -2,18 +2,47 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\NiveauEtudeRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
+
+#[ApiResource(
+    denormalizationContext: ['groups' => 'post:create:niveau_etude'],
+    normalizationContext: ['groups' => ['read:get:niveau_etude', 'read:collection:niveau_etude']],
+    operations: [
+        new GetCollection(
+            security: 'is_granted("ROLE_SUPER_ADMIN")'
+        ),
+        new Post(
+            security: 'is_granted("ROLE_SUPER_ADMIN")',
+            validationContext: ['groups' => ['post:create:validator']]
+        )
+    ]
+)]
 #[ORM\Entity(repositoryClass: NiveauEtudeRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_NIVEAU_ETUDE', fields: ['niveau_etude'])]
+#[UniqueEntity(fields: ["niveau_etude"], groups: ["post:create:validator"])]
 class NiveauEtude
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[
+        ORM\Column,
+        Groups(['read:get:niveau_etude', 'read:collection:niveau_etude'])
+    ]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[
+        ORM\Column(length: 255, unique: true),
+        Groups(['read:get:niveau_etude', 'read:collection:niveau_etude', 'post:create:niveau_etude']),
+        NotBlank(['groups' => ['post:create:validator']])
+    ]
     private ?string $niveau_etude = null;
 
     public function getId(): ?int
@@ -28,7 +57,7 @@ class NiveauEtude
 
     public function setNiveauEtude(string $niveau_etude): static
     {
-        $this->niveau_etude = $niveau_etude;
+        $this->niveau_etude = strtoupper($niveau_etude);
 
         return $this;
     }
---
+++ tests/src/Controller/NiveauEtude/PostNiveauEtudeControllerTest.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Tests\src\Controller\NiveauEtude;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Symfony\Bundle\Test\Client;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use App\Entity\User;
+use App\Traits\FixturesTrait;
+
+class PostNiveauEtudeControllerTest extends ApiTestCase
+{
+    use RefreshDatabaseTrait;
+    use FixturesTrait;
+
+    private ?Client $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->client = $this->createClient();
+        $this->loadFixturesTrait();
+    }
+    /**
+     * @dataProvider getUserAuthorized
+     */
+    public function testPostAuthorizedNiveauEtude(?string $roles, bool $access): void
+    {
+        /** @var User */
+        $user = $this->getUser($roles);
+        if ($roles) {
+            $this->client->loginUser($user);
+        }
+
+        $this->client->request('POST', '/api/niveau_etudes', [
+            "json" => [
+                'niveau_etude' => 'bacc'
+            ]
+        ]);
+
+        if (!$access) {
+            $status = 401;
+            if ($roles) {
+                $status = 403;
+            }
+            $this->assertResponseStatusCodeSame($status);
+        } else {
+            $this->assertResponseStatusCodeSame(201);
+            $this->assertJsonContains([
+                "niveau_etude" => "BACC"
+            ]);
+        }
+    }
+
+    /**
+     * @dataProvider getDataValid
+     */
+    public function testPostValidDataNiveauEtude(array $data): void
+    {
+        $this->myLogUser();
+
+        $this->client->request('POST', '/api/niveau_etudes', [
+            "json" => $data
+        ]);
+
+        if ($data['niveau_etude']) {
+            $this->assertResponseStatusCodeSame(201);
+        } else {
+            $this->assertResponseStatusCodeSame(422);
+        }
+    }
+
+    public function testUniqueNiveauEtude(): void
+    {
+        $this->myLogUser();
+
+        $this->client->request('POST', '/api/niveau_etudes', [
+            "json" => [
+                'niveau_etude' => 'unique'
+            ]
+        ]);
+
+        $this->assertResponseStatusCodeSame(422);
+    }
+
+    public static function getDataValid(): array
+    {
+        return [
+            "valid" => [["niveau_etude" => 'bacc']],
+            'blank' => [["niveau_etude" => '']]
+        ];
+    }
+
+    private function getUser(?string $roles): ?User
+    {
+        /** @var User */
+        $user = match ($roles) {
+            'super' => $this->all_fixtures['super'],
+            'admin' => $this->all_fixtures['admin'],
+            'user' => $this->all_fixtures['user_1'],
+            default => null
+        };
+
+        return $user;
+    }
+
+    public static function getUserAuthorized(): array
+    {
+        return [
+            'super' => ['roles' => 'super', 'access' => true],
+            'admin' => ['roles' => 'admin', 'access' => false],
+            'user' => ['roles' => 'user', 'access' => false],
+            'anonymous' => ['roles' => null, 'access' => false]
+        ];
+    }
+
+    public static function getData(): array
+    {
+        return [
+            'valid' => [['niveau_etude' => 'bacc']],
+            'null' => [['niveau_etude' => null]],
+            'blank' => [['niveau_etude' => '']],
+        ];
+    }
+
+    private function myLogUser(): void
+    {
+        /** @var User */
+        $super = $this->all_fixtures['super'];
+        $this->client->loginUser($super);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->client = null;
+    }
+}
