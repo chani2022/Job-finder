12c0e1d63cf31ff57fd3debf73e65f0a10db01f7
Chani Andrianarinaivo <chaniandrianarinaivo@gmail.com>
2025-05-26 16:40:15 +0100

    tests(offreEmploi): test de method get

---
+++ tests/src/Controller/OffreEmploi/GetOffreEmploiControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\src\Controller\OffreEmploi;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Symfony\Bundle\Test\Client;
+use App\Traits\FixturesTrait;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+
+class GetOffreEmploiControllerTest extends ApiTestCase
+{
+    use RefreshDatabaseTrait;
+    use FixturesTrait;
+
+    private ?Client $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->client = $this->createClient();
+    }
+
+    public function testGetOffre(): void
+    {
+        $offre_emploi = $this->all_fixtures['offre_emploi'];
+        $this->client->request('GET', '/api/offre_emplois/' . $offre_emploi->getId());
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->client = null;
+    }
+}
