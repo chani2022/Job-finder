7d50e52168b546ead61113aa4b0bd89c95386ad1
Chani Andrianarinaivo <chaniandrianarinaivo@gmail.com>
2025-06-16 17:22:35 +0100

    refonte: refonte de candidature processor pour un test unitaire

--- src/State/Processor/Candidature/CandidatureProcessor.php
+++ src/State/Processor/Candidature/CandidatureProcessor.php
@@ -4,37 +4,129 @@ namespace App\State\Processor\Candidature;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
-use ApiPlatform\Validator\Exception\ValidationException;
+use Symfony\Component\Validator\Exception\ValidatorException;
 use App\Entity\Candidature;
 use App\Entity\MediaObject;
 use App\Entity\OffreEmploi;
 use App\Entity\PieceJointe;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use InvalidArgumentException;
+use LogicException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CandidatureProcessor implements ProcessorInterface
 {
-    public function __construct(private readonly EntityManagerInterface $em, private ValidatorInterface $validator, private readonly TokenStorageInterface $tokenStorage) {}
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private ValidatorInterface $validator,
+        private readonly TokenStorageInterface $tokenStorage,
+        private UserProviderInterface $userProvider
+    ) {}
 
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): Candidature|ValidationException
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): Candidature|ValidatorException
     {
-        /** @var User  */
-        $user = $this->tokenStorage->getToken()->getUser();
-        $user = $this->em->getRepository(User::class)->find($user->getId());
-
-        $request = $context['request'];
-        $file = $request->files->get('file');
-        $id_offreEmploi = $request->request->get('id_offre');
-        $lettre = $request->request->get('lettre');
-
-        $offreEmploi = $this->em->getRepository(OffreEmploi::class)->find($id_offreEmploi);
-
+        $user = $this->getAuthenticatedUser();
         $candidature = new Candidature();
-        $candidature->setCandidat($user)
-            ->setOffreEmploi($offreEmploi);
 
+        if ($this->checkKeyRequestFromContext($context)) {
+            if ($this->checkKeysRequest($context['request'])) {
+                $data = $this->getDataRequest($context['request']);
+
+                $lettre = $data['lettre'];
+                $id_offreEmploi = $data['id_offre'];
+                $file = $data['file'];
+
+                $candidature = $this->setCandidature($user, $id_offreEmploi, $lettre, $file);
+                $this->validate($candidature);
+
+                $this->save($candidature);
+            }
+        }
+
+        return $candidature;
+    }
+    /** 
+     * @return User|NotFoundHttpException
+     */
+    public function getAuthenticatedUser()
+    {
+        $token = $this->tokenStorage->getToken();
+
+        if (!$token || !($user = $token->getUser()) instanceof User) {
+            throw new LogicException('Aucun utilisateur authentifié.');
+        }
+
+        return $this->userProvider->refreshUser($user);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function checkKeyRequestFromContext(array $context): bool
+    {
+        if (array_key_exists('request', $context)) {
+            return true;
+        }
+        throw new InvalidArgumentException('la clé request n\'est pas dans la request');
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function checkKeysRequest(Request $request): bool
+    {
+        $KeysValid = ['id_offre', 'lettre', 'file'];
+        foreach ($KeysValid as $key) {
+            $hasKey = $key === 'file'
+                ? $request->files->has($key)
+                : $request->request->has($key);
+
+            if (!$hasKey) {
+                throw new InvalidArgumentException("La clé obligatoire \"$key\" est manquante.");
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @throws ValidatorException
+     */
+    public function validate(Candidature $candidature): bool
+    {
+        $errors = $this->validator->validate($candidature, null, ['groups' => 'post:validator']);
+        if ($errors->count() > 0) {
+            throw new ValidatorException($errors);
+        }
+        return true;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function getDataRequest(Request $request): array
+    {
+        $id_offre = $request->request->get('id_offre');
+        $lettre = $request->request->get('lettre');
+        $file = $request->files->get('file');
+
+        if (is_null($id_offre) || is_null($lettre)) {
+            throw new InvalidArgumentException('Paramètre manquants');
+        }
+        return [
+            'id_offre' => $id_offre,
+            'lettre' => $lettre,
+            'file' => $file
+        ];
+    }
+
+    public function setCandidature(User $user, string $id_offreEmploi, string $lettre, UploadedFile $file): Candidature
+    {
         $media = new MediaObject();
         $media->file = $file;
 
@@ -43,16 +135,19 @@ class CandidatureProcessor implements ProcessorInterface
             ->setOwner($user)
             ->setCv($media);
 
-        $candidature->setPieceJointe($pieceJointe);
+        $offreEmploi = $this->em
+            ->getRepository(OffreEmploi::class)
+            ->find($id_offreEmploi);
 
-        $errors = $this->validator->validate($candidature, null, ['groups' => 'post:validator']);
-        if ($errors->count() > 0) {
-            throw new ValidationException($errors);
-        }
+        return (new Candidature())
+            ->setCandidat($user)
+            ->setOffreEmploi($offreEmploi)
+            ->setPieceJointe($pieceJointe);
+    }
 
+    public function save(Candidature $candidature): void
+    {
         $this->em->persist($candidature);
         $this->em->flush();
-
-        return $candidature;
     }
 }
--- tests/src/State/Processor/Candidature/CandidatureProcessorTest.php
+++ tests/src/State/Processor/Candidature/CandidatureProcessorTest.php
@@ -4,89 +4,379 @@ namespace App\Tests\src\State\Processor\Candidature;
 
 use ApiPlatform\Metadata\Post;
 use App\Entity\Candidature;
-use App\Entity\MediaObject;
-use App\Entity\PieceJointe;
-use App\Repository\CandidatureRepository;
 use App\State\Processor\Candidature\CandidatureProcessor;
-use App\Traits\FixturesTrait;
-use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
 use App\Entity\OffreEmploi;
-use App\Traits\LogUserTrait;
+use App\Repository\OffreEmploiRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use InvalidArgumentException;
+use LogicException;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Exception\ValidatorException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class CandidatureProcessorTest extends KernelTestCase
+class CandidatureProcessorTest extends TestCase
 {
-    use RefreshDatabaseTrait;
-    use FixturesTrait;
-    use LogUserTrait;
 
-    private Container $container;
+    /** @var MockObject|EntityManagerInterface*/
+    private $em = null;
+    /** @var MockObject|ValidatorInterface */
+    private $validator;
+    private ?TokenStorage $tokenStorage = null;
+    private ?CandidatureProcessor $candidatureProcessor = null;
+    /** @var UserProviderInterface|null|MockObject */
+    private $userProvider = null;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->container = static::getContainer();
-        $this->loadFixturesTrait();
+        $this->em = $this->createMock(EntityManagerInterface::class);
+        $this->validator = $this->createMock(ValidatorInterface::class);
+        $this->tokenStorage = new TokenStorage();
+        $this->userProvider = $this->createMock(UserProviderInterface::class);
+
+        $this->candidatureProcessor = new CandidatureProcessor($this->em, $this->validator, $this->tokenStorage, $this->userProvider);
     }
 
-    public function testCandidatureProcessor(): void
+    public function testCandidatureProcessorSuccess(): void
     {
-        /** @var OffreEmploi */
-        $offreEmploi = $this->all_fixtures['offre_emploi'];
-        /** @var User */
-        $user = $this->all_fixtures['user_adm_society'];
+        $operation = new Post();
+        $data = [];
+        $context = [];
 
-        $this->logUserTrait($user);
+        $user = new User();
+        $token = new UsernamePasswordToken($user, 'jwt');
+        $this->tokenStorage->setToken($token);
+        $this->userProvider->expects($this->once())
+            ->method('refreshUser')
+            ->with($user)
+            ->willReturn($user);
 
-        $request = new Request();
-        //data
-        $request->request->set('lettre', 'une lettre de motivation');
-        $request->request->set('id_offre', $offreEmploi->getId());
-        //file
-        $filename = 'upload.pdf';
-        $filepath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $filename;
-        file_put_contents($filepath, '%PDF-1.4 fake PDF content');
+        $dataRequest = [
+            'id_offre' => 1,
+            'lettre' => 1
+        ];
+        $dataFile = [
+            'file' => $this->createMock(UploadedFile::class)
+        ];
 
-        /**
-         * pour ne pas effacer le fichier dans le dossier fixtures
-         * lors de l'instance de uploadedfile
-         */
-        // copy($path_source_file, $tmp);
-        $uploadedFile = new UploadedFile($filepath, $filename, 'application/pdf', null, true);
-        $request->files->set('file', $uploadedFile);
-        $context['request'] = $request;
+        $context['request'] = new Request([], $dataRequest, [], [], $dataFile);
 
-        /** @var EntityManagerInterface */
-        $em = $this->container->get(EntityManagerInterface::class);
-        $validator = $this->container->get(ValidatorInterface::class);
-        $tokenStorage = $this->container->get(TokenStorageInterface::class);
-        $candidatureProcessor = new CandidatureProcessor($em, $validator, $tokenStorage);
+        $lettre = $context['request']->request->get('id_offre');
+        $id_offreEmploi = $context['request']->request->get('id_offre');
+        $file = $context['request']->files->get('file');
 
-        $res = $candidatureProcessor->process(null, new Post(), [], $context);
-        /** @var Candidature */
-        $candidature_bdd = $em->getRepository(Candidature::class)->find($res->getId());
-        $filename_bdd = $candidature_bdd->getPieceJointe()->getCv()->filePath;
+        $offreEmploi = new OffreEmploi();
+        $offreEmploiRepository = $this->createMock(OffreEmploiRepository::class);
+        $this->em
+            ->method('getRepository')
+            ->with(OffreEmploi::class)
+            ->willReturn($offreEmploiRepository);
+        $offreEmploiRepository
+            ->method('find')
+            ->with($id_offreEmploi)
+            ->willReturn($offreEmploi);
 
-        $this->assertNotNull($res->getId());
-        $this->assertEquals($candidature_bdd->getCandidat()->getId(), $user->getId());
+        $candidature = $this->candidatureProcessor->setCandidature($user, $id_offreEmploi, $lettre, $file);
 
-        // effacement des fichier uploader
-        $path_dest = static::getContainer()->getParameter('path_dest_images_test');
-        $pathFilename_dest = $path_dest . DIRECTORY_SEPARATOR . $filename_bdd;
-        $this->assertFileExists($pathFilename_dest);
-        unlink($pathFilename_dest);
-        $this->assertFileDoesNotExist($pathFilename_dest);
+        $constraintViolationList = $this->createMock(ConstraintViolationListInterface::class);
+        $this->validator->expects($this->once())
+            ->method('validate')
+            ->with($candidature)
+            ->willReturn($constraintViolationList);
+
+        $this->em->expects($this->once())
+            ->method('persist')
+            ->with($candidature);
+
+        $this->em->expects($this->once())
+            ->method('flush');
+
+        $res = $this->candidatureProcessor->process($data, $operation, [], $context);
+
+        $this->assertInstanceOf(Candidature::class, $res);
+        $this->assertEquals($offreEmploi, $res->getOffreEmploi());
+        $this->assertEquals($lettre, $res->getPieceJointe()->getLettreMotivation());
+        $this->assertEquals($file, $res->getPieceJointe()->getCv()->file);
+        $this->assertEquals($user, $res->getCandidat());
+        $this->assertEquals($user, $res->getPieceJointe()->getOwner());
     }
+
+    public function testCandidatureProcessorThrowException(): void
+    {
+        $operation = new Post();
+        $data = [];
+        $context = [];
+
+        $user = new User();
+        $token = new UsernamePasswordToken($user, 'jwt');
+        $this->tokenStorage->setToken($token);
+        $this->userProvider->expects($this->once())
+            ->method('refreshUser')
+            ->with($user)
+            ->willReturn($user);
+
+        $dataRequest = [
+            'id_offre' => 1,
+            'lettre' => 1
+        ];
+        $dataFile = [
+            'file' => $this->createMock(UploadedFile::class)
+        ];
+
+        $context['request'] = new Request([], $dataRequest, [], [], $dataFile);
+
+        $lettre = $context['request']->request->get('id_offre');
+        $id_offreEmploi = $context['request']->request->get('id_offre');
+        $file = $context['request']->files->get('file');
+
+        $offreEmploi = new OffreEmploi();
+        $offreEmploiRepository = $this->createMock(OffreEmploiRepository::class);
+        $this->em
+            ->method('getRepository')
+            ->with(OffreEmploi::class)
+            ->willReturn($offreEmploiRepository);
+        $offreEmploiRepository
+            ->method('find')
+            ->with($id_offreEmploi)
+            ->willReturn($offreEmploi);
+
+        $candidature = $this->candidatureProcessor->setCandidature($user, $id_offreEmploi, $lettre, $file);
+
+        $constraintViolationList = $this->createMock(ConstraintViolationListInterface::class);
+        $constraintViolationList->method('count')->willReturn(1);
+
+        $this->validator->expects($this->once())
+            ->method('validate')
+            ->with($candidature, null, ['groups' => 'post:validator'])
+            ->willReturn($constraintViolationList);
+        $this->expectException(ValidatorException::class);
+        $this->candidatureProcessor->process($data, $operation, [], $context);
+    }
+
+
+    public function testGetAuthenticatedUserSuccess(): void
+    {
+        $user = (new User());
+        $token = new UsernamePasswordToken($user, 'jwt');
+        $this->tokenStorage->setToken($token);
+        $this->userProvider->expects($this->once())
+            ->method('refreshUser')
+            ->with($user)
+            ->willReturn($user);
+
+        $res = $this->candidatureProcessor->getAuthenticatedUser();
+
+        $this->assertSame($res, $user);
+    }
+
+    public function testGetAuthenticatedUserThrowException(): void
+    {
+        $this->expectException(LogicException::class);
+
+        $this->candidatureProcessor->getAuthenticatedUser();
+    }
+
+    public function testCheckKeyRequestFromContextSuccess(): void
+    {
+        $context['request'] = new Request();
+        $requestIn = false;
+        if (array_key_exists('request', $context)) {
+            $requestIn = true;
+        }
+        $this->candidatureProcessor->checkKeyRequestFromContext($context);
+        $this->assertTrue($requestIn);
+    }
+
+    public function testCheckKeyRequestFromContextThrowException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->candidatureProcessor->checkKeyRequestFromContext([]);
+    }
+
+    public function testCheckKeysInRequestSuccess(): void
+    {
+        $request = new Request([], [
+            'id_offre' => '',
+            'lettre' => ''
+        ], [], [], [
+            'file' => $this->createMock(UploadedFile::class)
+        ]);
+
+        $res = $this->candidatureProcessor->checkKeysRequest($request);
+        $this->assertTrue($res);
+    }
+    /**
+     * @dataProvider getDataInvalid
+     */
+    public function testCheckKeysThrowException(array $data): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $request = new Request([], $data, [], [
+            'file' => $this->createMock(UploadedFile::class)
+        ], []);
+
+        $this->candidatureProcessor->checkKeysRequest($request);
+    }
+
+    public static function getDataInvalid(): array
+    {
+        return [
+            'data lettre missing ' => [
+                [
+                    'id_offre' => 1,
+                ]
+            ],
+            'data id_offre missing' => [
+                [
+                    'lettre' => 'lettre'
+                ]
+            ],
+        ];
+    }
+    public function testValidatorCandidatureNotException(): void
+    {
+        $candidature = new Candidature();
+        $violations = new ConstraintViolationList();
+
+        $this->validator->expects($this->once())
+            ->method('validate')
+            ->willReturn($violations);
+
+        $res = $this->candidatureProcessor->validate($candidature);
+
+        $this->assertTrue($res);
+    }
+
+    public function testGetDataRequestSuccess(): void
+    {
+        $request = new Request([], [
+            'id_offre' => 1,
+            'lettre' => 'lettre'
+        ], [], [], [
+            'file' => $this->createMock(UploadedFile::class)
+        ]);
+
+        $res = $this->candidatureProcessor->getDataRequest($request);
+
+        $this->assertEquals(1, $res['id_offre']);
+        $this->assertEquals('lettre', $res['lettre']);
+        $this->assertInstanceOf(UploadedFile::class, $res['file']);
+    }
+    /**
+     * @dataProvider getDataRequestInvalid
+     */
+    public function testGetDataRequestThrowException(array $data): void
+    {
+        $request = new Request([], $data, [], [], [
+            'file' => $this->createMock(UploadedFile::class)
+        ]);
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->candidatureProcessor->getDataRequest($request);
+    }
+
+    public static function getDataRequestInvalid(): array
+    {
+        return [
+            'id_offreEmploi null' => [
+                [
+                    'id_offre' => null,
+                    'lettre' => 'lettre',
+                ]
+            ],
+            'lettre missing' => [
+                [
+                    'id_offre' => 1,
+                    'lettre' => null,
+                ]
+            ],
+            'lettre et id_offre missing' => [
+                [
+                    'id_offre' => null,
+                    'lettre' => null,
+                ]
+            ]
+        ];
+    }
+
+
+    public function testValidatorCandidatureThrowException(): void
+    {
+        $candidature = new Candidature();
+        $violation = $this->createMock(ConstraintViolation::class);
+        $violations = new ConstraintViolationList([$violation]);
+
+        $this->validator->expects($this->once())
+            ->method('validate')
+            ->willReturn($violations);
+
+        $this->expectException(ValidatorException::class);
+
+        $this->candidatureProcessor->validate($candidature);
+    }
+
+    public function testSetCandidature(): void
+    {
+        $user = new User();
+        $id_offreEmploi = 1;
+        $lettre = 'lettre';
+        $file = $this->createMock(UploadedFile::class);
+        $offreEmploi = new OffreEmploi();
+
+        $offreEmploiRepository = $this->createMock(OffreEmploiRepository::class);
+
+        $this->em->expects($this->once())
+            ->method('getRepository')
+            ->with(OffreEmploi::class)
+            ->willReturn($offreEmploiRepository);
+
+        $offreEmploiRepository->expects($this->once())
+            ->method('find')
+            ->with($id_offreEmploi)
+            ->willReturn($offreEmploi);
+
+        $candidature = $this->candidatureProcessor->setCandidature($user, $id_offreEmploi, $lettre, $file);
+
+        $this->assertEquals($user, $candidature->getCandidat());
+        $this->assertEquals($offreEmploi, $candidature->getOffreEmploi());
+        $this->assertEquals($lettre, $candidature->getPieceJointe()->getLettreMotivation());
+        $this->assertEquals($user, $candidature->getPieceJointe()->getOwner());
+        $this->assertEquals($file, $candidature->getPieceJointe()->getCv()->file);
+    }
+
+    public function testSave(): void
+    {
+        $candidature = new Candidature();
+        $this->em->expects($this->once())
+            ->method('persist')
+            ->with($this->callback(fn($excepted) => $excepted instanceof Candidature));
+
+        $this->em->expects($this->once())
+            ->method('flush');
+
+        $this->candidatureProcessor->save($candidature);
+    }
+
+
 
     protected function tearDown(): void
     {
         parent::tearDown();
+        $this->em = null;
+        $this->validator = null;
+        $this->tokenStorage = null;
+        $this->candidatureProcessor = null;
     }
 }
